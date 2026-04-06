6d4f2985f958c39ab0f676b4bcccf0082eeab9cd
sachinmalekar96 <sachinmalekar96@gmail.com>
2026-04-06 09:52:35 +0530

    Update region in S3 backend configuration from us-east-1 to us-east-2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "sachin-terraform-backend-2026"
     key = "noteapp-ec2/state.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     profile = "default"
   }
 }
